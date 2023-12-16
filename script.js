42fef22d6a7d9d8f4ec9b1c2198607723d10fb3c
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2023-12-16 22:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
-function inCrementCounter() {
-	var crementElement = document.getElemetById('counter');
-	var crementValue = parseInt(crementElement.innerText);
-	alert('un-incremented value :' + crementValue);
-	crementElement.innerText = crementValue + 1;
-}
+// function inCrementCounter() {
+// 	var crementElement = document.getElemetById('counter');
+// 	var crementValue = parseInt(crementElement.innerText);
+// 	alert('un-incremented value :' + crementValue);
+// 	crementElement.innerText = crementValue + 1;
+// }
+
